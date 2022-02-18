99f4b7f51df2dc4c784aaa760bf85fcb919dfea1
anderson-santos-sidia <anderson.santos@sidia.com>
2022-02-18 14:57:43 -0300

    first commit

---
+++ src/components/LinkEstilizado/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LinkEstilizado = React.forwardRef(({onClick, href, children}, ref) => {
+    return (
+        <a href={href} onClick={onClick} ref={ref} style={{color:'red', cursor: 'pointer'}}>
+            {children}
+        </a>
+    );
+});
+export default LinkEstilizado;
